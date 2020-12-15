ad9986dc6cc8b738043c70c32ac69faf72c1183b
Saul Amster <saulamster@gmail.com>
2020-12-14 23:10:11 -0500

    completely revamped color picker

---
+++ Components/BrushOptionsMenu.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableHighlight } from 'react-native';
+import { TriangleColorPicker } from 'react-native-color-picker';
+import Slider from '@react-native-community/slider';
+import { toHsv, fromHsv } from 'react-native-color-picker'
+import styles from '../Styles/styles';
+// import { useState } from 'react';
+
+
+export default function BrushOptionsMenu(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [stringColor, setStringColor] = useState("#774488");
+    const { strokeWidth, onColorChange, onStrokeWidthChange } = props;
+
+    function colorButton() {
+        return (
+            <TouchableHighlight
+                style={{
+                    position: 'absolute',
+                    top: 45,
+                    right: 25,
+                    height: 40,
+                    width: 40,
+                    backgroundColor: stringColor,
+                    borderRadius: 20,
+                    borderColor: '#000000',
+                    borderWidth: 3
+                }}
+                onPress={() => setIsOpen(!isOpen)}
+            >  
+                <View></View>
+            </TouchableHighlight>
+        );
+    }
+
+    return (
+        isOpen ? 
+            [<View style={styles.colorSelection}>
+
+                <Text>Color: {stringColor.toUpperCase()}</Text>
+                <TriangleColorPicker
+                    style={styles.colorTriangle}
+                    defaultColor={stringColor}
+                    onColorChange={(color) => {onColorChange(fromHsv(color)); setStringColor(fromHsv(color))}}
+                />
+                <Text>Stroke Width: {Math.round(strokeWidth)}</Text>
+                <Slider
+                    minimumValue={10}
+                    maximumValue={100}
+                    value={strokeWidth}
+                    onValueChange={(value) => onStrokeWidthChange(value)}
+                />
+                
+            </View>,
+            colorButton()
+            ]
+        :
+            colorButton()
+    );
+}
+
--- Components/DrawingCanvas.js
+++ Components/DrawingCanvas.js
@@ -4,39 +4,32 @@ import { StyleSheet, View } from 'react-native';
 import { TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
 import {Picker} from '@react-native-picker/picker';
 import styles from '../Styles/styles';
-
+import BrushOptionsMenu from './BrushOptionsMenu';
 
 const DrawingCanvas = () => {
 
-    const [strokeColor, setStrokeColor] = useState(0x000000);
+    const [strokeColor, setStrokeColor] = useState(0x774488);
+    const [strokeWidth, setStrokeWidth] = useState(35);
+
     return (
         <View style={styles.canvas}>
             <ExpoPixi.Sketch
                 style={styles.canvas}
                 strokeColor={strokeColor}
-                strokeWidth={35}
-                strokeAlpha={0.5}
+                strokeWidth={strokeWidth}
+                strokeAlpha={1}
             />
             {/* <View style={styles.colorSelection}>
                 <TouchableOpacity onPress={()=>setStrokeColor()}>
 
                 </TouchableOpacity>
             </View> */}
-            <View>
-                <Picker
-                    selectedValue={strokeColor}
-                    onValueChange={(itemValue, itemIndex) => setStrokeColor(itemValue)}
-                    style={{height: 200}}>
-                    <Picker.Item label="Black" value={0x000000}></Picker.Item>
-                    <Picker.Item label="Red" value={0xff0000}></Picker.Item>
-                    <Picker.Item label="Blue" value={0x0000ff}></Picker.Item>
-                    <Picker.Item label="Green" value={0x00ff00}></Picker.Item>
-                    <Picker.Item label="Yellow" value={0xffff00}></Picker.Item>
-                    <Picker.Item label="Purple" value={0x800080}></Picker.Item>
-                </Picker>
-            </View>
+            <BrushOptionsMenu
+                strokeWidth={strokeWidth}
+                onColorChange={(color) => setStrokeColor(parseInt(color.substring(1), 16))}
+                onStrokeWidthChange={(value) => setStrokeWidth(value)}
+            />
         </View>
-        
     );
 }
 
